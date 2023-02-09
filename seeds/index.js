6c8316e07e9b4e4470452d153b3f2dd4c8519cd7
Emoses95 <moses.e.collins@gmail.com>
2023-02-09 14:33:37 -0800

    seeds progress

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const{User,Post,Comment}= require('../models')
 
 const seed = async()=>{
     await sequelize.sync({force:true});
-    const users = await User.bulkCreat([
+    const users = await User.bulkCreate([
         {
             email:'yaycomics@yay.com',
             password:'bookies'
@@ -19,7 +19,28 @@ const seed = async()=>{
     const post= await Post.bulkCreate([
         {
             post:'must read commic book',
-            
+            UserId:3
+
+        },
+        {
+            post:'long live comic bookies',
+            UserId:1
+        }
+    ],{
+        individualHooks:true
+    })
+
+    const comment= await Comment.bulkCreate([
+        {
+            comment:'better than batman',
+            UserId:3
+        },
+        {
+            comment:'batmans nothing with out robbin',
+            UserId:1
         }
     ])
+    process.exit(1)
 }
+
+seed();
