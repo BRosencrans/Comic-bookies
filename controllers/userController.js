35b86327d345d0135ea8c18b3f3cbcb62c2e55c9
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-02-09 13:13:15 -0800

    updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,4 +11,52 @@ router.get('/', (req, res)=>{
         res.status(500).json({msg:'aww shucks!', err})
     })
 })
-router.get('/:id', (req,res))
+router.get('/:id', (req,res)=>{
+    User.findByPk(req.params.id,{
+        include:[Post]
+    }).then(userData=>{
+        res.json(userData)
+    }).catch(err=>{
+        console.log(err)
+        res.status(500).json({msg:"aww shucks!", err})
+    })
+})
+router.post('/', (req,res)=>{
+    console.log(req.body);
+    User.create({
+        email:req.body.email,
+        password:req.body.password,
+        userName:req.body.userName
+    }).then(userData=>{
+        req.session.userId = userData.id;
+        req.session.userEmail = userData.email;
+        res.json(userData)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:'aww shucks!', err})
+    })
+})
+router.post('/login', (req,res)=>{
+    User.findOne({
+        where:{
+            email:req.body.email
+        }
+    }).then(userData=>{
+        if (!userData){
+            return res.status(403).json({msg:'incorrect login credentials'})
+        } else {
+            if(bcrypt.compareSync(req.body.password, userData.password)){
+                req.session.userId = userData.id;
+                req.session.userEmail = userData.email;
+                return res.json(userData)
+            } else {
+                return res.status(403).json({msg:'incorrect login credentials'})
+            }
+        }
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:'aww shucks!', err})
+        })
+    })
+
+    module.exports = router;
