7c963a7e2d008d4807b0e156e13a5174d5f00ec2
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-02-09 19:28:27 -0800

    wrote seeds for publisher table

--- seeds/index.js
+++ seeds/index.js
@@ -1,36 +1,30 @@
 const sequelize =require('../config/connection')
-const{User,Post,Comment}= require('../models')
+const{User,Post,Comment, Publisher}= require('../models')
 
 const seed = async()=>{
     await sequelize.sync({force:true});
     const users = await User.bulkCreate([
         {
             email:'yaycomics@yay.com',
-            password:'bookies',
-            userName: 'HwrdtheDuck',
-            user_id:1
+            password:'bookies'
         },
         {
             email:'welove@comicbooks',
-            password:'everybodyshouldreadcomicbooks',
-            userName: 'NotSuperman',
-            user_id:2
+            password:'everybodyshouldreadcomicbooks'
         }
     ],{
         individualHooks:true
     })
 
     const post= await Post.bulkCreate([
-        {title: "thoughts?",
-            text:'the best place to read comics is in an ice palace',
-            user_id:2,
-            userName: 'NotSuperman'
+        {
+            post:'must read commic book',
+            UserId:3
 
         },
-        {title: "best movie eveeer",
-            text:"when's the new howard the duck movie coming out?",
-            user_id:1,
-            userName: 'HwrdtheDuck'
+        {
+            post:'long live comic bookies',
+            UserId:1
         }
     ],{
         individualHooks:true
@@ -39,14 +33,68 @@ const seed = async()=>{
     const comment= await Comment.bulkCreate([
         {
             comment:'better than batman',
-            user_id:2,
-            post_id: 1
+            UserId:3
         },
         {
-            comment:'batmans nothing with out robbin',
-            user_id:1,
-            post_id:2
-
+            comment:'batmans nothing with out robin',
+            UserId:1
+        }
+    ])
+    const publisher = await Publisher.bulkCreate([
+        {
+            name:'DC Comics'
+        },{
+            name:"Marvel"
+        },{
+            name:"Dark Horse Comics"
+        },{
+            name:"Archie Comics"
+        },{
+            name:"Gold Key"
+        },{
+            name:"Heavy Metal"
+        },{
+            name:"Fiction house"
+        },{
+            name:"Quality Comics"
+        },{
+            name:'Fox Comics'
+        },{
+            name:"Ace Magazines"
+        },{
+            name:"Fawcett Publications"
+        },{
+            name:"Charlton"
+        },{
+            name:"Atlas Comics"
+        },{
+            name:"Image"
+        },{
+            name:'Max Comics'
+        },{
+            name:'Aircel Publishing'
+        },{
+            name:'Crossgen'
+        },{
+            name:'Eros Comix'
+        },{
+            name:"America's Best Comics"
+        },{
+            name:'Amerotica'
+        },{
+            name:'Avatar Press'
+        },{
+            name:'Tangent Comics'
+        },{
+            name:'Eurotica'
+        },{
+            name:'Wildstorm'
+        },{
+            name:'Verotik'
+        },{
+            name:'Milestone'
+        },{
+            
         }
     ])
     process.exit(1)
