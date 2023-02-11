b0b9d44c0b4ef3f3fff83b72e90c3480037d64bf
Kairo97 <109006341+kairo97@users.noreply.github.com>
2023-02-10 20:07:18 -0800

    working on models

---
+++ models/Series.js
@@ -0,0 +1,41 @@
+const {Model, DataTypes} = require('sequelize');
+sequelize = require('../config/connection');
+
+class Series extends Model {}
+
+Series.init({
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    aliases:{
+        type:DataTypes.STRING,
+        allowNull:true,
+    },
+    count_of_episodes:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+    },
+    first_episode_title:{
+        type:DataTypes.STRING,
+        allowNull:false,
+
+    },
+    last_episode_title:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    production:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    deck:{
+        type:DataTypes.STRING,
+        allowNull:true,
+
+    },
+    start_year:{
+        type:DataTypes.STRING
+    }
+})
+module.exports=Series
