67858aca07a2af4ab4c5628f9eb46f73ade416de
Emoses95 <moses.e.collins@gmail.com>
2023-02-12 00:28:20 -0800

    fix error

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -3,22 +3,24 @@ const { rmSync } = require('fs');
 const router= express.Router();
 const {User,Post, Comment,Character,Series,Publisher,Volume} = require('../models');
 
-router.get("/",(req,res)=>{
-    res.render("home")
-})
 
-// // login page
-// router.get("/login",(req,res)=>{
-//     if(req.session.loggedIn){
-//         return res.redirect("/")
-//     }
-//     res.render("login",{
-//         isLoggedIn:req.session.loggedIn,
-//         userId:req.session.userId,
-//     })
+
+// router.get("/",(req,res)=>{
+//     res.render("home")
 // })
 
+// // // login page
+// // router.get("/login",(req,res)=>{
+// //     if(req.session.loggedIn){
+// //         return res.redirect("/")
+// //     }
+// //     res.render("login",{
+// //         isLoggedIn:req.session.loggedIn,
+// //         userId:req.session.userId,
+// //     })
+// // })
 
 
-module.exports= router;
+
+// module.exports= router;
 
