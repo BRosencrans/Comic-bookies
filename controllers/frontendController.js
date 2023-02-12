f075adb9cd0bbddaa18a0bb6f8641a1e37ee9740
Emoses95 <moses.e.collins@gmail.com>
2023-02-12 00:17:00 -0800

    frontEndController User

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -1,41 +1,27 @@
 const express= require('express');
+const { rmSync } = require('fs');
 const router= express.Router();
-const {User, Post, Comment, Publisher} = require('./models');
-
+const {User,Post, Comment,Character,Series,Publisher,Volume} = require('../models');
 
 router.get("/",(req,res)=>{
-    Post.findAll({
-        include:[User,Post,Comment,Publisher]
-    }).then(postData=>{
-        console.log(postData)
-        const hbsPost = postData.map(post=>post.toJSON())
-        console.log('==============================')
-        console.log(hbsPost)
-        res.render("home",{
-            allPosts:hbsPosts
-        })
-    })
-})
-router.get("/login",(req,res)=>{
-    res.render("login")
-})
-router.get("/signup",(req,res)=>{
-    res.render("signup")
-})
-router.get("/profile",(req,res)=>{
-    if(!req.session.userId){
-        return res.redirect("/login")
+    res.render("home"){
+        isLoggedIn:req.session.logged
+        userId:req.session.userId,rmSync
     }
-    User.findByPk(req.session.userId,{
-        include:[Post,Comment,Publisher]
-    }).then(userdata=>{
-        console.log(userdata)
-        const hbsData = userdata.toJSON();
-        console.log('==============================')
-        console.log(hbsData)
-        res.render("profile",hbsData)
-    })
-    // res.redirect("/sessions")
 })
 
-module.exports = router;
+// // login page
+// router.get("/login",(req,res)=>{
+//     if(req.session.loggedIn){
+//         return res.redirect("/")
+//     }
+//     res.render("login",{
+//         isLoggedIn:req.session.loggedIn,
+//         userId:req.session.userId,
+//     })
+// })
+
+
+
+module.exports= router;
+
--- controllers/index.js
+++ controllers/index.js
@@ -14,12 +14,15 @@ const publisherRoutes = require('./publisherController')
 router.use('/api/publishers', publisherRoutes);
 
 const characterRoutes = require('./characterController')
-router.use('/api/characters', characterRoutes);
+router.use('/api/characters', characterRoutes); 
 
 // const seriesRoutes = require('./seriesController')
 // router.use('/api/series', seriesRoutes);
 
 const volumeRoutes = require('./volumeController')
 router.use('/api/volumes', volumeRoutes);
+
+const frontEndRoutes= require("./frontendController");
+router.use("/frontEndRoutes")
 
 module.exports = router;
