a50b83f70b30f8f32134edf452119f48267bef65
BRosencrans <brandon.rosencrans@gmail.com>
2023-02-11 17:15:12 -0800

    Route fixing

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -12,6 +12,15 @@ router.get('/', (req,res)=>{
         res.status(500).json({msg:"aww shucks!"})
     })
 })
+router.get('/:name', (req,res)=>{
+    Character.findOne(req.params.name,{
+    }).then(oneChar=>{
+        res.json(oneChar)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"aww shucks!", err})
+    })
+})
 router.get('/:id', (req,res)=>{
     Character.findByPk(req.params.id,{
     }).then(oneChar=>{
@@ -21,5 +30,7 @@ router.get('/:id', (req,res)=>{
         res.status(500).json({msg:"aww shucks!", err})
     })
 })
+
+
 
 module.exports = router;
