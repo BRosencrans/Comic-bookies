b0137bfcc6c7da48aa922a01755bf462f1e6d14b
Emoses95 <moses.e.collins@gmail.com>
2023-02-12 14:03:08 -0800

    work on publisher render

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -1,23 +1,38 @@
 const express =  require('express');
 const router = express.Router();
-const {User,Post,Comment,Publisher} = require('../models');
+const {Publisher} = require('../models');
 
-router.get('/', (req,res)=>{
-    Publisher.findAll().then(bookData=>{
-        res.json(bookData)
-    }).catch(err=>{
-        console.log(err);
-        res.status(500).json({msg:"aww shucks!", err})
-    })
+router.get('/', async (req,res)=>{
+    try{
+        const publisher= await Publisher.findAll();
+        
+        const publishers= []
+        for(let i=0; i < publishers.length; i++){
+            const pubObj= publishers[i];
+            const pubCounts= {
+                pubObj.toJSON(),
+                
+            }
+        }
+    }
 })
-router.get('/:id', (req,res)=>{
-    Publisher.findByPk(req.params.id,{
-    }).then(bookData=>{
-        res.json(bookData)
-    }).catch(err=>{
-        console.log(err);
-        res.status(500).json({msg:"aww shucks!", err})
-    })
-})
+
+// router.get('/', (req,res)=>{
+//     Publisher.findAll().then(bookData=>{
+//         res.json(bookData)
+//     }).catch(err=>{
+//         console.log(err);
+//         res.status(500).json({msg:"aww shucks!", err})
+//     })
+// })
+// router.get('/:id', (req,res)=>{
+//     Publisher.findByPk(req.params.id,{
+//     }).then(bookData=>{
+//         res.json(bookData)
+//     }).catch(err=>{
+//         console.log(err);
+//         res.status(500).json({msg:"aww shucks!", err})
+//     })
+// })
 
 module.exports = router
